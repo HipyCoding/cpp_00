d3b2e889cffb586326ac903065d80527058dc507
Candrese <candrese@student.42Heilbronn.de>
2025-01-30 18:45:21 +0100

    used transform() which is a forbidden function of the <algorithm> library. replaced it with my own function

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,20 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 03:01:32 by christian         #+#    #+#             */
-/*   Updated: 2025/01/20 17:54:34 by christian        ###   ########.fr       */
+/*   Updated: 2025/01/30 18:41:44 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <algorithm>
+
+// using std::cout;
+// using std::endl;
+// std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+
+std::string capitalize(std::string str)
+{
+	size_t i = 0;
+	while ( i < str.length())
+	{
+		str[i] = std::toupper(str[i]);
+		i++;
+	}
+	return str;
+}
 
 int main(int argc, char **argv)
 {
 	int	i = 1;
+
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -25,8 +40,7 @@ int main(int argc, char **argv)
 	while (i < argc)
 	{
 		std::string str(argv[i]);
-		std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-		std::cout << str;
+		std::cout << capitalize(str);
 		i++;
 	}
 	std::cout << std::endl;
